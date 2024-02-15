f9878ba819747db66f230f69aed48a73bbd23d9d
g-savant <42213922+g-savant@users.noreply.github.com>
2024-02-15 14:41:35 -0500

    range finder added

--- src/chip.sv
+++ src/chip.sv
@@ -9,39 +9,70 @@ module my_chip (
     
     // Basic counter design as an example
 
+    RangeFinder#(10) rf(.data_in(io_in[9:0]),
+                        .clock,
+                        .reset,
+                        .go(io_in[11]),
+                        .finish(io_in[10]),
+                        .range(io_out[9:0],
+                        .debug_error(io_out[11])));
 
-    wire [6:0] led_out;
-    assign io_out[6:0] = led_out;
+endmodule
 
-    // external clock is 1000Hz, so need 10 bit counter
-    reg [9:0] second_counter;
-    reg [3:0] digit;
 
-    always @(posedge clock) begin
-        // if reset, set counter to 0
-        if (reset) begin
-            second_counter <= 0;
-            digit <= 0;
+module RangeFinder
+  #(parameter WIDTH=16)
+  (input logic [WIDTH-1:0] data_in,
+  input logic clock, reset,
+  input logic go, finish,
+  output logic [WIDTH-1:0] range,
+  output logic debug_error);
+
+
+
+  logic[WIDTH-1:0] max_val, min_val;
+
+  logic going;
+
+  always_ff @(posedge clock, posedge reset) begin
+    if(reset) begin
+      going <= 1'b0;
+      min_val <= 'd0;
+      max_val <= 'd0;
+      debug_error <= 1'b0;
+    end else begin
+      if(go | going) begin
+        if(go & ~going) begin
+          if(finish) debug_error <= 1'b1;
+          else begin
+            max_val <= data_in;
+            min_val <= data_in;
+            debug_error <= 1'b0;
+            going <= 1'b1;
+          end
         end else begin
-            // if up to 16e6
-            if (second_counter == 1000) begin
-                // reset
-                second_counter <= 0;
-
-                // increment digit
-                digit <= digit + 1'b1;
-
-                // only count from 0 to 9
-                if (digit == 9)
-                    digit <= 0;
-
-            end else
-                // increment counter
-                second_counter <= second_counter + 1'b1;
+          if(finish) going <= 1'b0;
+          else going <= 1'b1;
+          if(data_in < min_val) min_val <= data_in;
+          else min_val <= min_val;
+          if(data_in > max_val) max_val <= data_in;
+          else max_val <= max_val;
         end
+      end else begin
+        if(~going & finish) debug_error <= 1'b1;
+        min_val <= min_val;
+        max_val <= max_val;
+        going <= 1'b0;
+      end
     end
+  end
 
-    // instantiate segment display
-    seg7 seg7(.counter(digit), .segments(led_out));
+  always_comb begin
+    if(data_in < max_val & data_in > min_val) range = max_val - min_val;
+    else if(data_in < max_val & data_in < min_val) range = max_val - data_in;
+    else if(data_in > max_val & data_in > min_val) range = data_in - min_val;
+    else if(data_in > max_val & data_in < min_val) range = max_val - min_val;
+  end
+
 
 endmodule
